48b0921e35b22c93b0f346867c9962a25c80db44
DeanLai <chekinout@hotmail.com>
2021-03-28 22:44:01 +0800

    add Swiper feature

--- js/all.js
+++ js/all.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {    
+$(document).ready(function () {
     $('.barnav .baritem > a').click(function (event) {
         event.preventDefault();
         $(this).toggleClass('active');
@@ -36,32 +36,57 @@ $(document).ready(function () {
         $('.nav .barnav .baritem').toArray().forEach(function (e) {
             var target = $(e).find('a').attr('href');
 
-            var targetTop = typeof($(target).offset()) != 'undefined' ? $(target).offset().top:0;
+            var targetTop = typeof ($(target).offset()) != 'undefined' ? $(target).offset().top : 0;
             var targetHeight = $(target).outerHeight();
-            
-            if (targetTop != 0 && (scrollPosition >= (targetTop - (targetHeight / 2) - 50) && scrollPosition <= (targetTop + targetHeight))) {             
-                if (!$(e).hasClass('active')){
+
+            if (targetTop != 0 && (scrollPosition >= (targetTop - (targetHeight / 2) - 50) && scrollPosition <= (targetTop + targetHeight))) {
+                if (!$(e).hasClass('active')) {
                     $(e).find('a').addClass('active');
                 }
-            }else{
+            } else {
                 $(e).find('a').removeClass('active');
             }
         });
 
-        
-        if (scrollPosition >= backToTop){
-            if (!topFlag){
-                $('.back-to-top').css('opacity',1).css('visibility','visible');
-                topFlag=true;
-            }                        
-        }else{
-            $('.back-to-top').css('opacity',0).css('visibility','hidden');;
-            topFlag=false;
+
+        if (scrollPosition >= backToTop) {
+            if (!topFlag) {
+                $('.back-to-top').css('opacity', 1).css('visibility', 'visible');
+                topFlag = true;
+            }
+        } else {
+            $('.back-to-top').css('opacity', 0).css('visibility', 'hidden');;
+            topFlag = false;
         };
     });
 
-    $('.back-to-top').on('click',function(e){
+    $('.back-to-top').on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({scrollTop:0},700);
+        $('html, body').animate({ scrollTop: 0 }, 700);
     })
+
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        // autoplay: {
+        //     delay: 5000,
+        //   },
+    });
 })
